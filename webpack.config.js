362a0e1a861a2a8246c0d772c744982e51a8e8b1
anton_yatskevich <anton_yatskevich@mail.ru>
2019-03-17 16:33:50 +0300

    docs: add prod and dev webpack configurations

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,48 @@
-const HtmlWebPackPlugin = require('html-webpack-plugin');
 const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
-module.exports = {
-	entry: './client/index.js',
-	output: {
-		filename: 'bundle.js',
-		path: path.resolve(__dirname, 'public')
-	},
-	module: {
-		rules: [
-			{
-				test: /\.(js|jsx)$/,
-				exclude: /node_modules/,
-				use: {
-					loader: 'babel-loader'
-				}
-			},
-			{
-				test: /\.scss$/,
-				use: [ 'style-loader', 'css-loader', 'sass-loader' ]
-			},
-			{
-				test: /\.html$/,
-				use: {
-					loader: 'html-loader'
-				}
-			}
-		]
-	},
-	plugins: [
-		new HtmlWebPackPlugin({
-			template: './client/index.html',
-			filename: './index.html'
-		})
-	]
-};
+module.exports = (env, argv) => ({
+    entry: './client/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'public')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.scss$/,
+                use: ['style-loader', MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.html$/,
+                use: {
+                    loader: 'html-loader'
+                }
+            }
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebPackPlugin({
+            template: './client/index.html',
+            filename: './index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].css'
+        })
+    ],
+    devServer: {
+        contentBase: './public',
+        stats: 'errors-only'
+    },
+    devtool: argv.mode === 'development' ? 'source-map' : 'none'
+});
