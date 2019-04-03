c907da6eaf37def373cecfd9e5685711b4e12954
anton_yatskevich <anton_yatskevich@mail.ru>
2019-04-03 13:27:44 +0300

    add utils tests

--- client/src/containers/AppContainer/index.spec.js
+++ client/src/containers/AppContainer/index.spec.js
@@ -7,4 +7,10 @@ describe('AppContainer component', () => {
         const AppContainerComponent = shallow(<AppContainer />);
         expect(AppContainerComponent).toMatchSnapshot();
     });
+
+    it('should set selectedMovie to null if goToSearchHandler is called', () => {
+        const AppContainerComponent = shallow(<AppContainer />);
+        AppContainerComponent.instance().goToSearchHandler();
+        expect(AppContainerComponent.state().selectedMovie).toBeNull();
+    });
 });
