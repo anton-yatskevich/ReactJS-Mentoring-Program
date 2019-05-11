3cfd005d3c61f44aba2b646532cf40c4550481e8
anton_yatskevich <anton_yatskevich@mail.ru>
2019-05-11 19:56:36 +0300

    fix: first run issue

--- webpack.client.js
+++ webpack.client.js
@@ -1,5 +1,4 @@
 const path = require('path');
-const HtmlWebPackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
@@ -33,10 +32,6 @@ module.exports = {
     },
     plugins: [
         new CleanWebpackPlugin(),
-        new HtmlWebPackPlugin({
-            template: './client/index.html',
-            filename: './index.html'
-        }),
         new MiniCssExtractPlugin({
             filename: '[name].css'
         })
