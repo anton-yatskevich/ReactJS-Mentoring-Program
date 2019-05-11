726cdc62d8d37f0a77ef3bc4625c25162d629f89
anton_yatskevich <anton_yatskevich@mail.ru>
2019-05-11 18:02:18 +0300

    feat: clean input on HomePage

--- client/src/pages/HomePage/index.js
+++ client/src/pages/HomePage/index.js
@@ -1,8 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import noop from 'lodash.noop';
 import SearchPanel from '../../containers/SearchPanel';
+import setSearchQuery from '../../actions/setSearchQuery';
 
-const HomePage = () => (
-    <SearchPanel />
-);
+class HomePage extends Component {
+    componentDidMount() {
+        const { setQueryValue } = this.props;
+        setQueryValue('');
+    }
 
-export default HomePage;
+    render() {
+        return (
+            <SearchPanel />
+        );
+    }
+}
+
+HomePage.propTypes = {
+    setQueryValue: PropTypes.func
+};
+
+HomePage.defaultProps = {
+    setQueryValue: noop
+};
+
+export default connect(null, { setQueryValue: setSearchQuery })(HomePage);
